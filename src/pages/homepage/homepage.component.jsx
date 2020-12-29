09a48d959585f9072a709b4ffa20c24cffb5ea83
amit <amit.vivek59lala@gmail.com>
2020-12-29 23:26:49 +0530

    second day

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import './homepage.style.scss'
+import Directory from '../../components/directory/directory.component'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory></Directory>
+    </div>
+)
+
+export default HomePage;
